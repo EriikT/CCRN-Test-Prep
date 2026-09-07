2b3eb65cf6867ad554d2881c12feadd788756029
Erik <masteret@ymail.com>
2026-09-07 10:10:40 -0400

    Add offline PWA support

--- ccrn-quiz/vite.config.js
+++ ccrn-quiz/vite.config.js
@@ -1,7 +1,26 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['ecg/spheal.png'],
+      manifest: {
+        name: 'CCRN Practice Quiz',
+        short_name: 'CCRN Quiz',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/ecg/spheal.png',
+            sizes: '475x475',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
 })
